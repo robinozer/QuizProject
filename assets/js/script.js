1c980a30341c294c81ec88ec231e4d9e6fde9461
robinozer <robin.ozer@outlook.com>
2022-12-18 08:58:23 +0000

    add questions array to separate file and change variable type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,3 @@
-// Create array of quiz questions and answer options
-
-let QUESTIONS = [{
-    question: "What country has the highest life expectancy?",
-    option1: "Spain",
-    option2: "Norway",
-    option3: "Japan",
-    option4: "Hong Kong",
-    correctAnswer: "Hong Kong",
-    correctOption: 4
-},
-
-{
-    question: "What year was the United Nations established?",
-    option1: "1920",
-    option2: "1930",
-    option3: "1945",
-    option4: "1955",
-    correctAnswer: "1945",
-    correctOption: 3
-},
-
-{
-    question: "In what country would you find Mount Kilimanjaro?",
-    option1: "Tanzania",
-    option2: "Ghana",
-    option3: "Wakanda",
-    option4: "South Africa",
-    correctAnswer: "Tanzania",
-    correctOption: 1
-},
-
-{
-    question: "What is the largest Spanish-speaking city in the world?",
-    option1: "Barcelona, Spain",
-    option2: "Mexico City, Mexico",
-    option3: "Havana, Cuba",
-    option4: "Santo Domingo, Dominican Republic",
-    correctAnswer: "Mexico City, Mexico",
-    correctOption: 2
-},
-
-{
-    question: "Who famously crossed the Alps with elephants on the way to war with the Romans?",
-    option1: "Caesar",
-    option2: "Hannibal",
-    option3: "Cicero",
-    option4: "Spartacus",
-    correctAnswer: "Hannibal",
-    correctOption: 2
-},
-
-{
-    question: "In which country is the Armenian sacred mountain, mount Ararat, located?",
-    option1: "Armenia",
-    option2: "Iran",
-    option3: "Georgia",
-    option4: "Turkey",
-    correctAnswer: "Turkey",
-    correctOption: 4
-},
-
-{
-    question: "Which Sultan of the Ottoman Empire conquered Constantinople?",
-    option1: "Sultan Murad IV",
-    option2: "Suleiman the Magnificent",
-    option3: "Sultan Mehmed II",
-    option4: "Beyazit II",
-    correctAnswer: "Sultan Mehmed II",
-    correctOption: 3
-},
-]
 
 // create variables retrieving the DOM elements 
 
@@ -187,6 +115,7 @@ function clearSelectionOfOptions() {
 2. If answer is correct, alert user and increment userScore with 1
 3. If else, alert user
 4. Next button shows up when user clicks Submit button
+5. Restart button appears when all questions in the array have been answered.
 **/
 function onUserSubmit(event){
     toggleOptions(true);
@@ -221,7 +150,7 @@ function displayCorrectOption(currentQuestion) {
 function incrementScore() {
     console.log(userScore);
     userScore++;
-    userScore.innerText = ;
+    userScore = `${userScore}`;
 }
 
 /**
@@ -232,9 +161,7 @@ function incrementScore() {
 **/
 function onRestartQuiz() {
     startQuiz.classList.add("hide");
-    restartQuiz.classList.remove("hide");
-    questionArea.classList.remove("hide");
-    nextQuestion.classList.remove("hide");
+    reStartQuiz.classList.remove("hide");
 }
 
 /**
